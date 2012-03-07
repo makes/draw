a03a4b3b484dcff5f07a36417f0eeb9d3b290b1a
simo@elektroplan.fi <simo@elektroplan.fi>
2012-03-07 09:58:37 +0200

    Added creation of ui/__init__.py to compile script.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,6 @@
 call pyuic4 -o ui\draw_main_window.py ui\draw_main_window.ui
 call pyrcc4 -o ui\images_rc.py ui\images.qrc
+type NUL > ui\__init__.py
 pylupdate4 translate.pro
 lrelease translate.pro
 
